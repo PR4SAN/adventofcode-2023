49468763360d82a9cae78387d7f3052c89cf40d0
Suprasanna Ojha <ssojha95@gmail.com>
2023-12-05 15:28:39 +0530

    day 1 part 2 done

--- day-1/index.js
+++ day-1/index.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import fs, { writeFileSync } from 'fs';
 import path from 'path';
 
 const currentModuleUrl = new URL(import.meta.url);
@@ -23,7 +23,48 @@ function part1() {
         }
         sum += parseInt(first+last);
     }
-    console.log(sum);
 }
 
 part1();
+
+const numberMap = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five' : 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5' : 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9
+}
+
+function part2() {
+    let sum = 0;
+    for (let i = 0; i < input.length; i++) {
+      const values = [];
+      for (let j in numberMap) {
+        let index = -1;
+        while ((index = input[i].indexOf(j, index + 1)) !== -1) {
+          values.push(`${index}-${numberMap[j]}`);
+        }
+      }
+      values.sort((a, b) => a.split('-')[0] - b.split('-')[0]);
+      console.log(values);
+      writeFileSync('output.txt',  values[0].split('-')[1]+values[values.length-1].split('-')[1]+'\n', {flag: 'a'});
+      sum += parseInt(values[0].split('-')[1] + values[values.length - 1].split('-')[1]);
+    }
+    console.log(sum);
+  }
+  
+
+part2();
